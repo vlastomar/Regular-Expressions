096c51c654ce8bad65ff8603eef48d41b2fb2505
VSM <vlad24@abv.bg>
2020-08-08 22:50:01 +0300

    Create MatchFullName.java

---
+++ MatchFullName.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatchFullName {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+
+        String regex = "\\b[A-Z][a-z]+ [A-Z][a-z]+\\b";
+        Pattern patt = Pattern.compile(regex);
+        Matcher mat = patt.matcher(input);
+
+        while (mat.find()){
+            System.out.print(mat.group() + " ");
+        }
+    }
+}
